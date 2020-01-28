622a6d6641a35e9cd60f6f263f9e22790da51727
Sven <app@svensfinlay.tech>
2020-01-28 20:47:47 +0000

    created class for single 'editable' chatbot connection

--- nodes/delete_watson/deleteWatson.js
+++ nodes/delete_watson/deleteWatson.js
@@ -1,36 +1,27 @@
-
-const AssistantV1 = require('ibm-watson/assistant/v1');
-const {IamAuthenticator} = require('ibm-watson/auth');
-const assistant = new AssistantV1({
-    version: '2019-02-08',
-    authenticator: new IamAuthenticator({
-        apikey: 'mHBe7hP3EvS--SAOe8fBSDRhTp78W__ZOL7iqfjMzUvf',//change this api key to your to modify your own workspace
-    }),
-    url: 'https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/4cc1d037-5230-4352-b3e4-dd74ede3951c',
-});
-
+const Watson_API = require('../scripts/chatbot_fuctions.js');
+let wa = new Watson_API();
 let json;
 let workspaceid;
 
 
 
 
-module.exports = function (RED) {
+module.exports = function(RED) {
     function deleteWatson(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        node.on('input', function (msg) {
-            assistant.listWorkspaces()
+        node.on('input', function(msg) {
+            wa.assistant.listWorkspaces()
                 .then(res => {
-                    json =  JSON.stringify(res, null, 2);
+                    json = JSON.stringify(res, null, 2);
                     const object = JSON.parse(json);
-                    for(let i =0; i<object.result.workspaces.length; i++) {
+                    for (let i = 0; i < object.result.workspaces.length; i++) {
                         if (object.result.workspaces[i].name == msg.payload) {
                             console.log(object.result.workspaces[i].name);
                             const params = {
                                 workspaceId: object.result.workspaces[i].workspace_id,
                             };
-                            assistant.deleteWorkspace(params)
+                            wa.assistant.deleteWorkspace(params)
                                 .then(res => {
                                     console.log(JSON.stringify(res, null, 2));
                                 })
---
+++ nodes/scripts/chatbot_fuctions.js
@@ -0,0 +1,26 @@
+const AssistantV1 = require('ibm-watson/assistant/v1');
+
+const {
+    IamAuthenticator
+} = require('ibm-watson/auth');
+
+
+
+class Watson_API {
+    assistant = new AssistantV1({
+        version: '2019-02-08',
+        authenticator: new IamAuthenticator({
+            apikey: 'NYLBfhff5TKngBCwOxjfRp7dIipvFPm_v1yo_XlR_K7W', //change this api key to your to modify your own workspace
+        }),
+        url: 'https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/a20b257b-83f7-44a4-8093-2553e67aa381',
+    });
+
+
+    get assistant() {
+        return this.assistant;
+    }
+
+}
+
+
+module.exports = Watson_API;
