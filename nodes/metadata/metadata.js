c2af1552c6ab5e7bd9789b7c0b6a42e8d87eb434
Sven <app@svensfinlay.tech>
2020-01-29 08:11:55 +0000

    Siggi's work, inject node

--- nodes/metadata/metadata.js
+++ nodes/metadata/metadata.js
@@ -1,11 +1,46 @@
 module.exports = function(RED) {
-    function LowerCaseNode(config) {
-        RED.nodes.createNode(this, config);
+    function MetadataNode(n) {
+        RED.nodes.createNode(this, n);
+        this.payload = n.payload;
+        this.payloadType = n.payloadType;
         var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+
+        this.on("input", function(msg) {
+            msg.topic = this.topic;
+            if (this.payloadType === 'str') {
+                try {
+                    if (this.payloadType == null) {
+                        msg.payload = this.payload;
+                    } else if (this.payloadType === 'none') {
+                        msg.payload = "";
+                    } else {
+                        msg.payload = RED.util.evaluateNodeProperty(this.payload, this.payloadType, this, msg);
+                    }
+                    this.send(msg);
+                    msg = null;
+                } catch (err) {
+                    this.error(err, msg);
+                }
+            }
         });
     }
-    RED.nodes.registerType("metadata", LowerCaseNode);
+
+    RED.nodes.registerType("metadata", MetadataNode);
+
+    RED.httpAdmin.post("/metadata/:id", RED.auth.needsPermission("inject.write"), function(req, res) {
+        var node = RED.nodes.getNode(req.params.id);
+        if (node != null) {
+            try {
+                node.receive();
+                res.sendStatus(200);
+            } catch (err) {
+                res.sendStatus(500);
+                node.error(RED._("inject.failed", {
+                    error: err.toString()
+                }));
+            }
+        } else {
+            res.sendStatus(404);
+        }
+    });
 }
