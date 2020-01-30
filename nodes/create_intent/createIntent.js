3fa4a708d02a43819d1ecba6ddd8ee9ef97857ee
Jiahao Wang <jiahao.wang.19@ucl.ac.uk>
2020-01-30 15:51:53 +0000

    create new node to create new intent

---
+++ nodes/create_intent/createIntent.js
@@ -0,0 +1,57 @@
+const Watson_API = require('../scripts/chatbot_fuctions.js');
+let wa = new Watson_API();
+let json;
+let workspaceid;
+
+
+
+
+module.exports = function(RED) {
+    function createIntent(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function(msg) {
+            wa.assistant.listWorkspaces()
+                .then(res => {
+                    json = JSON.stringify(res, null, 2);
+                    const object = JSON.parse(json);
+                    for (let i = 0; i < object.result.workspaces.length; i++) {
+                        if (object.result.workspaces[i].name == msg.payload) {
+                            console.log(object.result.workspaces[i].name);
+                            const params = {
+								workspaceId: object.result.workspaces[i].workspace_id,
+								intent: 'hello',
+								examples: [
+										{
+										  text: 'Good morning'
+										},
+										{
+										  text: 'Hi there'
+										}
+									]
+								};
+
+							wa.assistant.createIntent(params)
+							  .then(res => {
+								console.log(JSON.stringify(res, null, 2));
+							  })
+							  .catch(err => {
+								console.log(err)
+							  });
+                        }
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+
+
+
+        });
+
+
+
+    }
+
+    RED.nodes.registerType("createIntent", createIntent);
+}
