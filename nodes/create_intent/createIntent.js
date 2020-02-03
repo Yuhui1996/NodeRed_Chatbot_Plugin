16ae6247959e237bb2453520ba84bb1c7485e75c
Jiahao Wang <jiahao.wang.19@ucl.ac.uk>
2020-02-03 17:14:13 +0000

    get id from previous node

--- nodes/create_intent/createIntent.js
+++ nodes/create_intent/createIntent.js
@@ -1,57 +1,34 @@
 const Watson_API = require('../scripts/chatbot_fuctions.js');
 let wa = new Watson_API();
-let json;
 let workspaceid;
-
-
-
 
 module.exports = function(RED) {
     function createIntent(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.on('input', function(msg) {
-            wa.assistant.listWorkspaces()
-                .then(res => {
-                    json = JSON.stringify(res, null, 2);
-                    const object = JSON.parse(json);
-                    for (let i = 0; i < object.result.workspaces.length; i++) {
-                        if (object.result.workspaces[i].name == msg.payload) {
-                            console.log(object.result.workspaces[i].name);
-                            const params = {
-								workspaceId: object.result.workspaces[i].workspace_id,
-								intent: config.name,
-								description: config.description,
-								examples: [
-										{
-										  text: config.example1
-										},
-										{
-										  text: config.example2
-										}
-									]
-								};
-
-							wa.assistant.createIntent(params)
-							  .then(res => {
-								console.log(JSON.stringify(res, null, 2));
-							  })
-							  .catch(err => {
-								console.log(err)
-							  });
-                        }
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-
-
-
+			const params = {
+				workspaceId: msg.payload,
+				intent: config.name,
+				description: config.description,
+				examples: [
+						{
+						  text: config.example1
+						},
+						{
+						  text: config.example2
+						}
+					]
+			}
+			wa.assistant.createIntent(params)
+			  .then(res => {
+				console.log(JSON.stringify(res, null, 2));
+				node.send(msg.payload);
+			  })
+			  .catch(err => {
+				console.log(err)
+			  });
         });
-
-
-
     }
 
     RED.nodes.registerType("createIntent", createIntent);
