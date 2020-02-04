92b4606e58fc92ba938cca85f9ad94000be3946c
Sven <app@svensfinlay.tech>
2020-02-04 17:05:11 +0000

    Missed part

--- nodes/create_watson/createWatson.js
+++ nodes/create_watson/createWatson.js
@@ -5,25 +5,26 @@ let json;
 module.exports = function(RED) {
 
 
-        function createWatson(config) {
-            RED.nodes.createNode(this, config);
-            var node = this;
-            node.on('input', function(msg) {
-                const workspace = {
-                    name: msg.payload.chatbot_name,
-                    description: 'this is the first chatbot created using node.js'
-                }
-                wa.assistant.createWorkspace(workspace)
-                    .then(res => {
-                        json = JSON.stringify(res, null, 2);
-                        let object = JSON.parse(json);
-                        workspaceid = object.result.workspace_id;
-                        node.send(workspaceid); //send workspace id to next
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    });
-            });
-        }
+    function createWatson(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function(msg) {
+            const workspace = {
+                name: msg.payload.chatbot_name,
+                description: 'this is the first chatbot created using node.js'
+            }
+            wa.assistant.createWorkspace(workspace)
+                .then(res => {
+                    json = JSON.stringify(res, null, 2);
+                    let object = JSON.parse(json);
+                    workspaceid = object.result.workspace_id;
+                    node.send(workspaceid); //send workspace id to next
+                })
+                .catch(err => {
+                    console.log(err)
+                });
+        });
+    }
 
-        RED.nodes.registerType("createWatson",
+    RED.nodes.registerType("createWatson", createWatson);
+}
