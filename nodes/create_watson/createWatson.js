3c6ede20a93c9e8c166a61cc1e1935e6b6e2e569
Sven <app@svensfinlay.tech>
2020-02-05 14:09:55 +0000

    fixed Tests

--- nodes/create_watson/createWatson.js
+++ nodes/create_watson/createWatson.js
@@ -1,12 +1,38 @@
+const AssistantV1 = require('ibm-watson/assistant/v1');
+const {
+    IamAuthenticator
+} = require('ibm-watson/auth');
+const api_origional = 'NYLBfhff5TKngBCwOxjfRp7dIipvFPm_v1yo_XlR_K7W';
+const instance_origional = 'https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/a20b257b-83f7-44a4-8093-2553e67aa381';
+
 let workspaceid;
 let json;
 module.exports = function(RED) {
 
 
+
     function createWatson(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        this.assistant = this.context().flow.get("assistant")
+
+        try {
+            this.assistant = this.context().flow.get("assistant");
+        } catch (e) {
+            console.log("context not found");
+        } finally {
+            if (this.assistant == null || this.assistant == undefined) {
+                this.assistant = new AssistantV1({
+                    version: '2019-02-08',
+                    authenticator: new IamAuthenticator({
+                        apikey: api_origional,
+                    }),
+                    url: instance_origional
+                });
+            }
+        }
+
+
+
         node.on('input', function(msg) {
             const workspace = {
                 name: msg.payload.chatbot_name,
