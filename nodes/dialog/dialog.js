895732b010eab059da64ae9e3b5d19863d8a4853
sbs61@hi.is <sbs61@hi.is>
2020-02-09 18:57:15 +0000

    some progress

---
+++ nodes/dialog/dialog.js
@@ -0,0 +1,37 @@
+const Watson_API = require('../scripts/chatbot_fuctions.js');
+let assistant = new Watson_API().assistant;
+
+
+module.exports = function(RED) {
+    function DialogNode(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        this.name = n.name;
+        this.value = n.value;
+        node.on('input', function(msg) {
+            console.log("msg: " + msg);
+            assistant.createEntity(
+                {
+                    workspaceId: msg,
+                    entity: this.name,
+                    values: [
+                    {
+                        value: this.value
+                    }
+                    ]
+                }
+            )
+                .then(res => {
+                console.log("name: " + this.name)
+                console.log(JSON.stringify(res, null, 2));
+                })
+            .catch(err => {
+                console.log(err)
+            });
+            msg.payload = "Entity created";
+            node.send(msg);
+        });
+    }
+
+    RED.nodes.registerType("dialog",DialogNode);
+}
