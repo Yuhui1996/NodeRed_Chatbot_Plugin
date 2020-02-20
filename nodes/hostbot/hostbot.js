7f0fb98b9e4d9f7851edbb15faf9fd1182bff407
UdomkarnBoonyaprasert <udomkarnb@gmail.com>
2020-02-20 11:16:09 +0000

    Add new dependencies Running the flows now open a new tab automatically. Add new var in url and also a readme

--- nodes/hostbot/hostbot.js
+++ nodes/hostbot/hostbot.js
@@ -2,8 +2,15 @@ module.exports = function(RED) {
     function hostbotNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        var open = require('open');//added to dependencies
         node.on('input', function(msg) {
-            msg.payload = "http://watsonchatbot.epizy.com/main/main.html?api=" + msg.payload['wa_api_key'] +"&&url=" + msg.payload['instance_url'];
+            url = "http://watsonchatbot.epizy.com/main/main.html?api=" + msg.payload['wa_api_key'] +"&&url=" + msg.payload['instance_url'];
+            if (typeof msg.payload['workspaceId'] !== "undefined"){//TODO: need to confirm this ===> [workspaceId]
+            	url += "&&workspace=" + msg.payload['workspaceId'];
+            }
+            msg.payload = url;
+            console.log("The link to your chatbot is, " + url + "     (please make sure to select the correct instance before chatting)")
+            open(url);
             node.send(msg);
         });
     }
