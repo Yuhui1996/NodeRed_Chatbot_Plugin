e9cc43efa463374b7ad303ba8b9117c8144ed723
UdomkarnBoonyaprasert <udomkarnb@gmail.com>
2020-03-04 21:08:18 +0000

    update main.html api key and url now get directly passed into the html file not through url link to look more 'user-friendly'

--- nodes/hostbot/hostbot.js
+++ nodes/hostbot/hostbot.js
@@ -7,12 +7,32 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
 
             var connect = require('connect');
+            var varsInHtml = 'passApi = "' + msg.payload['wa_api_key'] +'";\npassUrl = "' + msg.payload['instance_url'] + '";\n'
             var serveStatic = require('serve-static');
             connect().use(serveStatic(__dirname)).listen(8080, function(){
                 console.log('Server running on 8080...');
             });
             //url = "http://watsonchatbot.epizy.com/main/main.html?api=" + msg.payload['wa_api_key'] +"&&url=" + msg.payload['instance_url'];
-            url = "http://localhost:8080/main.html?api=" + msg.payload['wa_api_key'] +"&&url=" + msg.payload['instance_url'];
+            var fs = require("fs");
+            var path = require('path');
+            var filePath = path.join(__dirname, "main.html");
+            
+
+            fs.readFile(filePath, function(err, buf) {
+                var htmlString = buf.toString();
+                //var varsInHtml = "passApi = " + msg.payload['wa_api_key'] +";\npassUrl = " + msg.payload['instance_url'] + ";\n"
+                var newHtmlString = htmlString.replace("//arguments_for_WA_should_be_inserted_here", varsInHtml);
+
+                file = require('fs');
+                filePath = path.join(__dirname, "index.html");
+                file.writeFile(filePath, newHtmlString, function (err) {
+                  if (err) return console.log(err);
+                  console.log('hostbot: html ready');
+                });
+            });
+
+            //url = "http://localhost:8080/main.html?api=" + msg.payload['wa_api_key'] +"&&url=" + msg.payload['instance_url'];
+            url = "http://localhost:8080/index.html"
             if (typeof msg.payload['workspaceId'] !== "undefined"){//TODO: need to confirm this ===> [workspaceId]
             	url += "&&workspace=" + msg.payload['workspaceId'];
             }
