4888e650b6bda1a393a156529b8b7551d23eb9a2
UdomkarnBoonyaprasert <udomkarnb@gmail.com>
2020-03-10 18:01:19 +0000

    add code for chat widget node now send payload of the code that can be inserted to a website for the chat widget

--- nodes/hostbot/hostbot.js
+++ nodes/hostbot/hostbot.js
@@ -8,6 +8,9 @@ module.exports = function(RED) {
 
             var connect = require('connect');
             var varsInHtml = 'passApi = "' + msg.payload['wa_api_key'] +'";\npassUrl = "' + msg.payload['instance_url'] + '";\n'
+            if (typeof msg.payload.workspaceId !== "undefined"){//TODO: need to confirm this ===> [workspaceId]
+                varsInHtml += 'passWorkspace = "' + msg.payload.workspaceId +'";\n';
+            }
             var serveStatic = require('serve-static');
             connect().use(serveStatic(__dirname)).listen(8080, function(){
                 console.log('Server running on 8080...');
@@ -20,7 +23,6 @@ module.exports = function(RED) {
 
             fs.readFile(filePath, function(err, buf) {
                 var htmlString = buf.toString();
-                //var varsInHtml = "passApi = " + msg.payload['wa_api_key'] +";\npassUrl = " + msg.payload['instance_url'] + ";\n"
                 var newHtmlString = htmlString.replace("//arguments_for_WA_should_be_inserted_here", varsInHtml);
 
                 file = require('fs');
@@ -31,15 +33,23 @@ module.exports = function(RED) {
                 });
             });
 
+            filePath = path.join(__dirname, "chatbot_iframe_web_insert.txt");
+
             //url = "http://localhost:8080/main.html?api=" + msg.payload['wa_api_key'] +"&&url=" + msg.payload['instance_url'];
             url = "http://localhost:8080/index.html"
-            if (typeof msg.payload['workspaceId'] !== "undefined"){//TODO: need to confirm this ===> [workspaceId]
-            	url += "&&workspace=" + msg.payload['workspaceId'];
-            }
-            msg.payload = url;
+            
+
+            var iframe_snip = '';
+            fs.readFile(filePath, function(err, buf) {
+                var htmlString = buf.toString();
+                iframe_snip = htmlString.replace("<!--the_url_of_the_chatbot-->", url);
+                msg.payload = "<!--append this code to your website-->\n\n"+iframe_snip;
+                node.send(msg);
+            });
+
             console.log("The link to your chatbot is, " + url + "     (please make sure to select the correct instance before chatting)")
+            console.log('widget insert:  passed as payload');
             open(url);
-            node.send(msg);
         });
     }
     RED.nodes.registerType("hostbot",hostbotNode);
