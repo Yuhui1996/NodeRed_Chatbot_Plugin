d5cb46099c4c6578bfcd76b392054fab32fadeb9
Sven <app@svensfinlay.tech>
2020-03-15 23:10:38 +0000

    fixed bug

--- nodes/dialog/dialog.js
+++ nodes/dialog/dialog.js
@@ -86,14 +86,15 @@ module.exports = function (RED) {
             console.log(this.id);
 
             this.id = this.id + Math.random().toString(36).substr(2, 10);
+
             //for creating dialog node
 
-            function getResponses(){
+            function getResponses() {
                 var output = {
                     generic: []
                 }
                 console.log(n.response_values[0].responseContent);
-                for(var i = 0; i < n.response_values.length; i++){
+                for (var i = 0; i < n.response_values.length; i++) {
                     output.generic.push({
                         values: [
                             {
@@ -103,46 +104,48 @@ module.exports = function (RED) {
                         response_type: "text"
                     })
                 }
+                return output;
+            }
 
 
-                let params = {
-                    workspaceId: msg.payload.workspaceId,
-                    parent: msg.payload.nodeID,
-                     previous_sibling: addID(this.id),
-                    dialogNode: this.id, //needs to be unique
-                    conditions: getReferenceValue(n.dialog_type, n.dialog_value, n.condition, n.conditionChoices),
-                    title: n.name,
-                    output: getResponses(),
-                    nextStep: {
-                        behavior: n.userAction
-                    }
-                };
+            let params = {
+                workspaceId: msg.payload.workspaceId,
+                parent: msg.payload.nodeID,
+                previous_sibling: addID(this.id),
+                dialogNode: this.id, //needs to be unique
+                conditions: getReferenceValue(n.dialog_type, n.dialog_value, n.condition, n.conditionChoices),
+                title: n.name,
+                output: getResponses(),
+                nextStep: {
+                    behavior: n.userAction
+                }
+            };
 
 
-                // console.log(params);
-                let top = this;
+            // console.log(params);
+            let top = this;
 
 
-                promise_queue.addToQueue(() => top.assistant.createDialogNode(params))
-                    .then(res => {
+            promise_queue.addToQueue(() => top.assistant.createDialogNode(params))
+                .then(res => {
 
-                        json = JSON.stringify(res, null, 2);
-                        let object = JSON.parse(json);
-                        let nodeID = top.id;
-                        msg.payload.nodeID = nodeID;
-                        msg.payload.discovery_api_key=msg.payload.discovery_api_key;
-                        msg.payload.discoveryUrl= msg.payload.discoveryUrl;
-                        node.status({fill: "green", shape: "ring", text: "Complete"});
-                        node.send(msg);
+                    json = JSON.stringify(res, null, 2);
+                    let object = JSON.parse(json);
+                    let nodeID = top.id;
+                    msg.payload.nodeID = nodeID;
+                    msg.payload.discovery_api_key = msg.payload.discovery_api_key;
+                    msg.payload.discoveryUrl = msg.payload.discoveryUrl;
+                    node.status({fill: "green", shape: "ring", text: "Complete"});
+                    node.send(msg);
 
 
-                    })
-                    .catch(err => {
-                        console.log(err)
-                        this.status({fill: "red", shape: "ring", text: "failed"});
-                        //    "THIS IS ERROR OF" + this.id + "__________________________-\n\n" +
-                    });
-            });
+                })
+                .catch(err => {
+                    console.log(err)
+                    this.status({fill: "red", shape: "ring", text: "failed"});
+                    //    "THIS IS ERROR OF" + this.id + "__________________________-\n\n" +
+                });
+        });
     }
 
     RED.nodes.registerType("dialog", createDialog);
