c401ed682d7fea8ef1dadd03ef1622e0fe570d3d
UdomkarnBoonyaprasert <udomkarnb@gmail.com>
2020-03-15 11:53:22 +0000

    add global var for dialog discovery dialog and discovery map are now stored in a json file under node/hostbot/

--- nodes/dialog/dialog.js
+++ nodes/dialog/dialog.js
@@ -20,11 +20,11 @@ module.exports = function (RED) {
                     break;
                 }
                 case "is": {
-                    result = sendType + sendValue + ":" + relationValue;
+                    result = sendType + sendValue + " : " + relationValue;
                     break;
                 }
                 case "is_not": {
-                    result = sendType + sendValue + " != \"" + relationValue + "\"";
+                    result = sendType + sendValue + " != " + relationValue;
                     break;
                 }
                 case "greater": {
@@ -48,149 +48,65 @@ module.exports = function (RED) {
         this.name = n.name;
         this.intentName = n.intentName;
         this.intentDescription = n.intentDescription;
-        console.log("start dialog");
         node.on('input', function (msg) {
 
-                let self = this;
-
-                try {
-                    this.assistant = this.context().flow.get("assistant");
-                } catch (e) {
-                    console.log("context not found");
-                } finally {
-                    if (this.assistant == null || this.assistant == undefined) {
-                        this.assistant = new AssistantV1({
-                            version: '2019-02-08',
-                            authenticator: new IamAuthenticator({
-                                apikey: msg.payload.wa_api_key,
-                            }),
-                            url: msg.payload.instance_url
-                        });
-                    }
+            try {
+                this.assistant = this.context().flow.get("assistant");
+            } catch (e) {
+                console.log("context not found");
+            } finally {
+                if (this.assistant == null || this.assistant == undefined) {
+                    this.assistant = new AssistantV1({
+                        version: '2019-02-08',
+                        authenticator: new IamAuthenticator({
+                            apikey: msg.payload.wa_api_key,
+                        }),
+                        url: msg.payload.instance_url
+                    });
                 }
+            }
 
 
-                try {
+            try {
 
-                    if (this.context().flow.get("ids") != undefined) {
-                        this.id = this.context().flow.get("ids");
-                    } else {
-                        this.id = 1;
-                        this.context().flow.set("ids", this.id);
-                    }
-                    let nextID = this.id + 1;
-                    this.context().flow.set("ids", nextID);
-                } catch (e) {
+                if (this.context().flow.get("ids") != undefined) {
+                    this.id = this.context().flow.get("ids");
+                } else {
                     this.id = 1;
                     this.context().flow.set("ids", this.id);
                 }
-
-
-                function addID(newID) {
-
-                    console.log(n.name);
-
-                    let siblings = self.context().flow.get("siblings");
-                    let previous_siblings = "";
-                    if (siblings[msg.payload.nodeID] != undefined) {
-
-                        previous_siblings = siblings[msg.payload.nodeID].id;
-                        console.log("->  " + siblings[msg.payload.nodeID].name);
-                        siblings[msg.payload.nodeID] = {
-                            id: newID,
-                            name: n.name
-                        };
-
-                    } else {
-                        siblings[msg.payload.nodeID] = {
-                            id: newID,
-                            name: n.name
-                        };
-                    }
-
-                    self.context().flow.set("siblings", siblings);
-                    return previous_siblings;
-                }
-
-                this.id = this.id + Math.random().toString(36).substr(2, 10);
-
-                //for creating dialog node
-                function getResponses() {
-                    var output = {
-                        generic: []
-                    }
-
-                    var responses = n.dialog_response;
-                    for (var i = 0; i < responses.length; i++) {
-                        if (responses[i].response_type === "image") {
-                            var image = responses[i].image;
-                            var response = {};
-                            response.response_type = "image";
-
-                            //if(responses[i].url != undefined){
-                            response.source = image.source;
-                            //console.log("source:" + image.source);
-                            //}
-                            if (image.title != undefined) {
-                                response.title = image.title;
-                            }
-                            if (image.description != undefined) {
-                                response.description = image.description;
-                            }
-                            output.generic.push(response);
-                        } else if (responses[i].response_type === "text") {
-                            output.generic.push({
-                                values: [
-                                    {
-                                        text: responses[i].responseContent
-                                    }
-                                ],
-                                response_type: "text"
-                            })
-                        }
-                    }
-                    return output;
-                }
-
-
-                let params = {
-                    workspaceId: msg.payload.workspaceId,
-                    parent: msg.payload.nodeID,
-                     previous_sibling: addID(this.id),
-                    dialogNode: this.id, //needs to be unique
-                    conditions: getReferenceValue(n.dialog_type, n.dialog_value, n.condition, n.conditionChoices),
-                    title: n.name,
-                    output: getResponses(),
-                    nextStep: {
-                        behavior: n.userAction
-                    }
-                };
-
-
-                // console.log(params);
-                let top = this;
-
-
-                promise_queue.addToQueue(() => top.assistant.createDialogNode(params))
-                    .then(res => {
-
-                        json = JSON.stringify(res, null, 2);
-                        let object = JSON.parse(json);
-                        let nodeID = top.id;
-                        msg.payload.nodeID = nodeID;
-                        node.status({fill: "green", shape: "ring", text: "Complete"});
-                        node.send(msg);
-
-
-                    })
-                    .catch(err => {
-                        console.log(err)
-                        this.status({fill: "red", shape: "ring", text: "failed"});
-                        //    "THIS IS ERROR OF" + this.id + "__________________________-\n\n" +
-                    });
+                let nextID = this.id + 1;
+                this.context().flow.set("ids", nextID);
+            } catch (e) {
+                this.id = 1;
+                this.context().flow.set("ids", this.id);
             }
 
-            
+
+            console.log(this.id);
+
+            this.id = this.id + Math.random().toString(36).substr(2, 10);
+            //for creating dialog node
+
+            function getResponses(){
+                var output = {
+                    generic: []
+                }
+                console.log(n.response_values[0].responseContent);
+                for(var i = 0; i < n.response_values.length; i++){
+                    output.generic.push({
+                        values: [
+                            {
+                                text: n.response_values[i].responseContent
+                            }
+                        ],
+                        response_type: "text"
+                    })
+                }
+
+                return output;
+            }
+
             let params = {
                 workspaceId: msg.payload.workspaceId,
                 parent: msg.payload.nodeID,
@@ -222,7 +138,7 @@ module.exports = function (RED) {
                 .catch(err => {
                     console.log(err)
                     this.status({fill:"red",shape:"ring",text:"failed"});
-                    //    "THIS IS ERROR OF" + this.id + "__________________________-\n\n" +
+                    //    "THIS IS ERROR OF" + this.id + "__________-\n\n" +
                 });
         });
     }
---
+++ nodes/hostbot/hostbot.js
@@ -0,0 +1,56 @@
+module.exports = function(RED) {
+    function hostbotNode(config) {
+
+        RED.nodes.createNode(this,config);
+        var node = this;
+        var open = require('open');//added to dependencies
+        node.on('input', function(msg) {
+
+            var connect = require('connect');
+            var varsInHtml = 'passApi = "' + msg.payload['wa_api_key'] +'";\npassUrl = "' + msg.payload['instance_url'] + '";\n'
+            if (typeof msg.payload.workspaceId !== "undefined"){//TODO: need to confirm this ===> [workspaceId]
+                varsInHtml += 'passWorkspace = "' + msg.payload.workspaceId +'";\n';
+            }
+            var serveStatic = require('serve-static');
+            connect().use(serveStatic(__dirname)).listen(8080, function(){
+                console.log('Server running on 8080...');
+            });
+            //url = "http://watsonchatbot.epizy.com/main/main.html?api=" + msg.payload['wa_api_key'] +"&&url=" + msg.payload['instance_url'];
+            var fs = require("fs");
+            var path = require('path');
+            var filePath = path.join(__dirname, "main.html");
+            
+
+            fs.readFile(filePath, function(err, buf) {
+                var htmlString = buf.toString();
+                var newHtmlString = htmlString.replace("//arguments_for_WA_should_be_inserted_here", varsInHtml);
+
+                file = require('fs');
+                filePath = path.join(__dirname, "index.html");
+                file.writeFile(filePath, newHtmlString, function (err) {
+                  if (err) return console.log(err);
+                  console.log('hostbot: html ready');
+                });
+            });
+
+            filePath = path.join(__dirname, "chatbot_iframe_web_insert.txt");
+
+            //url = "http://localhost:8080/main.html?api=" + msg.payload['wa_api_key'] +"&&url=" + msg.payload['instance_url'];
+            url = "http://localhost:8080/index.html"
+            
+
+            var iframe_snip = '';
+            fs.readFile(filePath, function(err, buf) {
+                var htmlString = buf.toString();
+                iframe_snip = htmlString.replace("<!--the_url_of_the_chatbot-->", url);
+                msg.payload = "<!--append this code to your website-->\n\n"+iframe_snip;
+                node.send(msg);
+            });
+
+            console.log("The link to your chatbot is, " + url + "     (please make sure to select the correct instance before chatting)")
+            console.log('widget insert:  passed as payload');
+            open(url);
+        });
+    }
+    RED.nodes.registerType("hostbot",hostbotNode);
+}
